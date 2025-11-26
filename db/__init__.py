42856c64d530e31e49ef6d7095170f9da08c6114
realhowieb <hnbrownjr@gmail.com>
2025-11-26 11:26:35 -0800

    DB UI Updates v2.1

--- db/__init__.py
+++ db/__init__.py
@@ -1,6 +1,12 @@
 from .engine import get_db_status, get_neon_conn, get_sqlite_conn
 from .runs import save_run, save_daily_snapshot, list_runs, load_run_results
 from .users import load_users, fetch_all_users, seed_neon_users_from_local
+from .schema import (
+    ensure_neon_runs_schema,
+    ensure_sqlite_runs_schema,
+    ensure_neon_users_schema,
+    ensure_sqlite_users_schema,
+)
 
 __all__ = [
     "get_db_status",
@@ -13,4 +19,8 @@ __all__ = [
     "load_users",
     "fetch_all_users",
     "seed_neon_users_from_local",
+    "ensure_neon_runs_schema",
+    "ensure_sqlite_runs_schema",
+    "ensure_neon_users_schema",
+    "ensure_sqlite_users_schema",
 ]
