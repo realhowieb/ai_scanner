e90508ba5b06524dcc74d5450594873004edcb07
realhowieb <hnbrownjr@gmail.com>
2025-12-07 12:41:33 -0800

    MP AI Logo

--- ui/header.py
+++ ui/header.py
@@ -6,7 +6,7 @@ def render_header():
         """
         <style>
             .block-container {
-                padding-top: 0.3rem !important;
+                padding-top: 0rem !important;
             }
         </style>
         """,
